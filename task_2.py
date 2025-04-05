4949e500fde6b81f075df21f6413cf8ae78f0d0f
rechkalovam <bykova.mary93@yandex.ru>
2025-04-05 17:21:04 +0500

    исправление к первому комментарию

--- task_2.py
+++ task_2.py
@@ -9,13 +9,13 @@ class Movies:
 
 class Comedy(Movies):
     def add_movie(self, movie):
-        self.movies.append(movie)
+        super().add_movie(movie)
         return f" Комедии: {self.movies}"
 
 
 class Drama(Movies):
     def add_movie(self, movie):
-        self.movies.append(movie)
+        super().add_movie(movie)
         return f" Драмы: {self.movies}"
 
 
